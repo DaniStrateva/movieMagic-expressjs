3af1f0095e1a0278797a7d3695dbbadec542f9f9
DaniStrateva <daniystrateva@gmail.com>
2025-06-06 16:03:49 -0600

    Adding and configuration handlebars

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
+//init express instnace
 const app = express();
 
+//use factory method for express engine
+app.engine('hbs',handlebars.engine({
+    extname: 'hbs',
+}));
+
+//set default engine
+app.set('view engine','hbs');
+
+//configure routes
 app.get('/',(req,res)=>{
-    res.send('It works');
+    res.render('home',{layout: false})
 });
 
+//initiate express server
 app.listen(5000,()=>console.log('Server is listening on http://localhost:5000...'));
