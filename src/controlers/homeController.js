85ed0e8bb761ce0efe69d27e375376dd92072317
DaniStrateva <daniystrateva@gmail.com>
2025-06-06 20:42:55 -0600

    Create service for movies

--- src/controlers/homeController.js
+++ src/controlers/homeController.js
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import movieService from '../services/movieService.js';
 //new instance of modular router
 //make sure controller has appropriate name
 const homeController = express.Router();
 
 
 homeController.get('/',(req,res)=>{
+    const movies=movieService.getAllMovies();
+
     res.render('home')
 });
 
